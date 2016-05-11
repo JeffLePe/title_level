296da8a6c18f7972b82653ea82de3a288df3fa00
JeffLePe <jeffersonleon@yahoo.com.br>
2016-05-10 21:06:26 -0300

    java script com for

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 var h1 = document.createElement('h1');
 h1.innerHTML = "Simão";
 document.body.appendChild(h1);
+
+for(var i=2; i<7; i++){
+    console.log("Count: "+i);
+    var h = document.createElement('h'+i);
+    h.innerHTML = "Simão";
+    document.body.appendChild(h);
+}
